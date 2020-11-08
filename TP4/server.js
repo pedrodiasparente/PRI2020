700073fb0a0f5f3aad0f6707995df5fc0ef266db
pedrodiasparente <pdparente@gmail.com>
2020-11-08 15:02:09 +0000

    localhost:port tambem funciona como index

--- TP4/server.js
+++ TP4/server.js
@@ -2,7 +2,22 @@ var http = require('http')
 var fs = require('fs')
 
 var servidor = http.createServer(function(req,res) {
-    if(req.url == '/arqs/*') {
+    if(req.url == '/'){
+        fs.readFile('arqsite/index.html', function (err, data) {
+            if (err) {
+                console.log('' + err)
+                res.writeHead(200, { 'Content-Type': 'text/html' })
+                res.write("Error: Falha a ler o ficheiro")
+                res.end()
+            }
+            else {
+                res.writeHead(200, { 'Content-Type': 'text/html' })
+                res.write(data)
+                res.end()
+            }
+        })
+    }
+    else if(req.url == '/arqs/*') {
         fs.readFile('arqsite/index.html', function (err, data) {
             if (err) {
                 console.log('' + err)
